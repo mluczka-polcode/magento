856b4d057a1157d6d765acf8fd2f035a67822617
Maciej Łuczka <maciej.luczka@polcode.net>
2014-01-03 15:48:24 +0100

    slider

--- app/code/local/Mage/Catalog/Block/Product/Slider.php
+++ app/code/local/Mage/Catalog/Block/Product/Slider.php
@@ -2,25 +2,21 @@
 
 class Mage_Catalog_Block_Product_Slider extends Mage_Catalog_Block_Product_Abstract
 {
-//     public function __construct()
-//     {
-//         parent::__construct();
-//
-//         $storeId = Mage::app()->getStore()->getId();
-//
-//         $products = Mage::getResourceModel('reports/product_collection')
-// //             ->addOrderedQty()
-//             ->addAttributeToSelect(array('name', 'price', 'special_price', 'small_image', 'short_description', 'description'))
-//             ->setStoreId($storeId)
-//             ->addStoreFilter($storeId)
-// //             ->setOrder('ordered_qty', 'desc')
-//             ;
-//
-// //         Mage::getSingleton('catalog/product_status')->addVisibleFilterToCollection($products);
-// //         Mage::getSingleton('catalog/product_visibility')->addVisibleInCatalogFilterToCollection($products);
-//
-// //         $products->setPageSize(6)->setCurPage(1);
-//
-//         $this->setProductCollection($products);
-//     }
+    public function __construct()
+    {
+        parent::__construct();
+
+        $storeId = Mage::app()->getStore()->getId();
+
+        $products = Mage::getResourceModel('reports/product_collection')
+            ->addAttributeToSelect(array('image'))
+            ->setStoreId($storeId)
+            ->addStoreFilter($storeId)
+            ->addAttributeToFilter('show_in_slider', '1')
+            ;
+
+        Mage::getSingleton('catalog/product_status')->addVisibleFilterToCollection($products);
+
+        $this->setProductCollection($products);
+    }
 }
